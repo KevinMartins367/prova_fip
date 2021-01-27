fbcc617d71809ff7bab474cf7573b84769b10e8e
Kevin <kevincosta13679@outlook.com>
2021-01-26 23:03:31 -0300

    crud ususarios

--- application/models/Model_usuarios.php
+++ application/models/Model_usuarios.php
@@ -1,9 +1,43 @@
 <?php
 
-class Model_usuarios extends CI_Model {
-  
-  public function get_all() {
-    return $this->db->get('usuarios')
-                    ->result();
-  }  
+class Model_usuarios extends CI_Model
+{
+
+	public function get_all()
+	{
+		return $this->db->get('usuarios')
+			->result();
+	}
+
+	public function get($id)
+	{
+
+		$this->db->select('U.*');
+		$this->db->from('usuarios U');
+		$this->db->where('id', $id);
+		$query = $this->db->get();
+		return $query->result_array();
+	}
+
+	public function insert($data)
+	{
+		$this->db->insert('usuarios', $data);
+
+		// GET ID
+		$customer_id = $this->db->insert_id();
+		//RETURN ID
+		return $customer_id;
+	}
+
+	public function edit($id, $data)
+	{
+		$this->db->where('id', $id);
+		$this->db->update('usuarios', $data);
+	}
+
+	public function delete($id)
+	{
+		$this->db->where('id', $id);
+		$this->db->delete('usuarios');
+	}
 }
